5926e60ae0fc377e0b3faa9a495d51e36f23b902
BB9z <bb9z@me.com>
2020-01-05 08:47:38 +0800

    #ci script upgrade.

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#! /bin/zsh
 
 set -euo pipefail
 
@@ -14,10 +14,22 @@ logError () {
     echo "\033[31m$1\033[0m" >&2
 }
 
-logInfo "$TRAVIS_COMMIT_MESSAGE"
+# Make sure all parameters are set correctly.
+logInfo "RFCI_PRODUCT_NAME = $RFCI_PRODUCT_NAME"
+
+readonly RFCI_TASK="${RFCI_TASK:? is not set.}"
 logInfo "RFCI_TASK = $RFCI_TASK"
-readonly RFSTAGE="$1"
-logInfo "RFSTAGE = $RFSTAGE"
+
+readonly RFCI_STAGE="${1:?STAGE is not set.}"
+logInfo "RFCI_STAGE = $RFCI_STAGE"
+
+readonly RFWorkspace=${RFWorkspace:="$RFCI_PRODUCT_NAME.xcworkspace"}
+logInfo "RFWorkspace = $RFWorkspace"
+
+TRAVIS_COMMIT_MESSAGE=${TRAVIS_COMMIT_MESSAGE:="$(logWarning 'TRAVIS_COMMIT_MESSAGE is not set, leave it blank.')"}
+TRAVIS_BRANCH=${TRAVIS_BRANCH:="$(logWarning 'TRAVIS_BRANCH is not set, leave it blank.')"}
+
+echo ""
 
 # Run test
 # $1 scheme
@@ -45,8 +57,16 @@ STAGE_MAIN() {
         if [[ "$TRAVIS_COMMIT_MESSAGE" = *"[skip lint]"* ]]; then
             logWarning "Skip pod lint"
         else
-            logInfo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
-            pod lib lint
+            if [[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+ ]]; then
+                logWarning "Release the podspec."
+                pod trunk push "$RFCI_PRODUCT_NAME.podspec"
+            elif [ "$TRAVIS_BRANCH" = "master" ]; then
+                logInfo "Lint the podspec."
+                pod lib lint --fail-fast
+            else
+                logInfo "Lint the podspec."
+                pod lib lint --fail-fast --allow-warnings
+            fi
         fi
 
     elif [ "$RFCI_TASK" = "Xcode10" ]; then
@@ -71,4 +91,4 @@ STAGE_FAILURE() {
     fi
 }
 
-"STAGE_$RFSTAGE"
+"STAGE_$RFCI_STAGE"
